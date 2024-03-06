0204fac2f39ed5d1c71b02c38c046f0ba5a929f3
Aidar <ajydarbakytov@gmail.com>
2024-03-06 10:55:36 +0600

    homePageProfile

--- src/routes/MainRoutes.jsx
+++ src/routes/MainRoutes.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import HomePage from "../pages/HomePage/HomePage";
+import Profile from "../components/profile/Profile";
 
 export default function MainRoutes() {
   const PUBLIC_ROUTES = [
     {
       path: "/",
       element: <HomePage />,
+    },
+    {
+      path: "/profile",
+      element: <Profile />,
     },
   ];
   return (
